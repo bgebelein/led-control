f817eedd7fb5693bf3294ab96348444e847fa2f5
bgebelein <bent.gebelein@akquinet.de>
2023-05-08 15:05:30 +0200

    refactored UI, refactored WebSocket stuff

--- data/script.js
+++ data/script.js
@@ -1,5 +1,5 @@
 // Websocket Connection
-/*
+
 var gateway = `ws://${window.location.hostname}/ws`;
 var websocket;
 window.addEventListener('load', onload);
@@ -7,6 +7,8 @@ window.addEventListener('load', onload);
 function onload(event) {
     initWebSocket();
 }
+
+// Callback functions
 
 function initWebSocket() {
     console.log('Trying to open a WebSocket connection…');
@@ -39,32 +41,34 @@ function onMessage(event) {
 
     keys.forEach(function(key){
         switch (key) {
-            case "modeValue":
-                document.querySelector('input[value=' + ledObj[key] + ']').checked = true;
+            case "mode":
+                document.querySelector(`input[value="${ledObj[key]}"]`).checked = true;
                 break;
-            case "colorValue":
+            case "temp":
+                temperatureInput.value = ledObj[key];
+                break;
+            case "hue1":
+                hueInput1.value = ledObj[key];
                 color.value = ledObj[key];
                 break;
-            case "brightnessValue":
-                brightness.value = ledObj[key];
+            case "hue2":
+                hueInput2.value = ledObj[key];
+                break;
+            case "lit":
+                brightnessInput.value = ledObj[key];
+                break;
+            case "speed":
+                speedInput.value = ledObj[key];
                 break;
             default:
                 console.log("Sorry, no keys available.");
         }
     });
 }
-*/
-
-
 
 /* ------------------------------------ UI Stuff ------------------------------------ */
 
 let currentMode = document.querySelector('input[type=radio]:checked').value;
-let hue1 = getComputedStyle(document.documentElement).getPropertyValue('--hue-1');
-let hue2 = getComputedStyle(document.documentElement).getPropertyValue('--hue-2');
-let sat = getComputedStyle(document.documentElement).getPropertyValue('--sat');
-let lit = getComputedStyle(document.documentElement).getPropertyValue('--lit');
-let speed = getComputedStyle(document.documentElement).getPropertyValue('--speed');
 
 const temperatureInput = document.querySelector('#temperature-input > input[type="range"]');
 const hueInput1 = document.querySelector('#hue-input-1 > input[type="range"]');
@@ -190,7 +194,7 @@ function setSpeed() {
 }
 
 function setBrightness() {
-    setCssProps(getCssProp('hue1'), getCssProp('hue2'), getCssProp('sat'), brightnessInput.value, getCssProp('hue2'));
+    setCssProps(getCssProp('hue1'), getCssProp('hue2'), getCssProp('sat'), brightnessInput.value, getCssProp('speed'));
 }
 
 btn.addEventListener('click', function() {
@@ -202,220 +206,11 @@ btn.addEventListener('click', function() {
     setBrightness();
 });
 
+/* ------------------------------------ Websocket Message ------------------------------------ */
 
-/*
-// Colormodes etc.
-
-let currentMode;
-let hue1 = getComputedStyle(document.documentElement).getPropertyValue('--hue-1');
-let hue2 = getComputedStyle(document.documentElement).getPropertyValue('--hue-2');
-let sat = getComputedStyle(document.documentElement).getPropertyValue('--sat');
-let lit = getComputedStyle(document.documentElement).getPropertyValue('--lit');
-let speed = getComputedStyle(document.documentElement).getPropertyValue('--speed');
-
-// Detect current mode
-function setCurrentMode(){
-    currentMode = document.querySelector('input[type=radio]:checked').value;
-    console.log('Current mode: ' + currentMode);
-
-    document.querySelectorAll('.cat').forEach(cat => cat.classList.remove('fade', 'breathe', 'sparkle'));
-
-    if (currentMode === 'white') {
-        setTemperature();
-    }
-
-    if (currentMode === 'color') {
-        setColor();
-    }
-
-    if (currentMode === 'gradient') {
-        setGradient();
-    }
-
-    if (currentMode === 'fade') {
-        document.querySelectorAll('.cat').forEach(cat => cat.classList.add('fade'));
-        setFadeSpeed();
-    }
-
-    if (currentMode === 'rainbow') {
-        document.querySelectorAll('.cat').forEach(cat => cat.classList.add('fade'));
-        setRainbowSpeed();
-    }
-
-    if (currentMode === 'breathe') {
-        document.querySelectorAll('.cat').forEach(cat => cat.classList.add('breathe'));
-        setBreathe();
-    }
-    
-    if (currentMode === 'motion') {
-        document.querySelectorAll('.cat').forEach(cat => cat.classList.add('breathe'));
-        setMotion();
-    }
-
-    if (currentMode === 'sparkle') {
-        document.querySelectorAll('.cat').forEach(cat => cat.classList.add('sparkle'));
-        setSparkleColor();
-    }
-    
-    setBrightness();
-    
-    websocket.send("modeValue" + currentMode.toString());
-}
-
-// Inputs
-const modes = document.querySelectorAll('input[type=radio]');
-const temperatureInput = document.querySelector('#temperature-value');
-const colorInput = document.querySelector('#color-value');
-const gradientValInput1 = document.querySelector('#gradient-value-1');
-const gradientValInput2 = document.querySelector('#gradient-value-2');
-const fadeSpeedInput = document.querySelector('#fade-value');
-const rainbowSpeed = document.querySelector('#rainbow-speed-value');
-const breatheColorInput = document.querySelector('#breathe-color-value');
-const breatheSpeedInput = document.querySelector('#breathe-speed-value');
-const motionColorInput = document.querySelector('#motion-color-value');
-const motionSpeedInput = document.querySelector('#motion-speed-value');
-const sparkleColorInput = document.querySelector('#sparkle-color-value');
-const sparkleSpeedInput = document.querySelector('#sparkle-speed-value');
-
-// Control modes
-
-modes.forEach(function(item) {
-    item.addEventListener('change', function(e) {
-        setCurrentMode();
+document.querySelectorAll('input').forEach(function(input){
+    input.addEventListener('change', function(){
+        console.log(`${input.name}: ${input.value}`);
+        websocket.send(input.name + input.value);
     });
 });
-
-function setTemperature() {
-    hue1 = 30;
-    document.documentElement.style.setProperty('--hue-1', hue1);
-
-    hue2 = 30;
-    document.documentElement.style.setProperty('--hue-2', hue2);
-
-    sat = (100 - Math.round(temperatureInput.value / 2.55)) + '%';
-    document.documentElement.style.setProperty('--sat', sat);
-
-    lit = (50 + Math.round(temperatureInput.value / 5.1)) + '%';
-    document.documentElement.style.setProperty('--lit', lit);
-
-    websocket.send("temperatureValue" + temperatureInput.value.toString());
-}
-
-// Set color
-function setColor() {
-    hue1 = Math.round(colorInput.value * 1.41176);
-    document.documentElement.style.setProperty('--hue-1', hue1);
-
-    hue2 = hue1;
-    document.documentElement.style.setProperty('--hue-2', hue2);
-
-    sat = '100%';
-    document.documentElement.style.setProperty('--sat', sat);
-
-    setBrightness();
-
-    websocket.send("colorValue" + colorInput.value.toString());
-}
-
-// Set gradient
-function setGradient() {
-    hue1 = Math.round(gradientValInput1.value * 1.41176);
-    document.documentElement.style.setProperty('--hue-1', hue1);
-
-    hue2 = Math.round(gradientValInput2.value * 1.41176);
-    document.documentElement.style.setProperty('--hue-2', hue2);
-
-    sat = '100%';
-    document.documentElement.style.setProperty('--sat', sat);
-
-    setBrightness();
-
-    websocket.send("gradientValue1" + gradientValInput1.value.toString());
-    websocket.send("gradientValue1" + gradientValInput2.value.toString());
-}
-
-// Set fade
-function setFadeSpeed() {
-    hue2 = hue1;
-    document.documentElement.style.setProperty('--hue-2', hue2);
-
-    sat = '100%';
-    document.documentElement.style.setProperty('--sat', sat);
-
-    document.documentElement.style.setProperty('--speed', 10000 - fadeSpeedInput.value + 'ms');
-
-    setBrightness();
-
-    document.documentElement.style.setProperty('--speed', 10000 - fadeSpeedInput.value + 'ms');
-}
-
-// Detect rainbow speed
-function setRainbowSpeed() {
-    hue2 = hue1 + 40;
-    document.documentElement.style.setProperty('--hue-2', hue2);
-
-    document.documentElement.style.setProperty('--speed', 10000 - rainbowSpeed.value + 'ms');
-
-    websocket.send("rainbowSpeedValue" + rainbowSpeed.value.toString());
-}
-
-// Set Breathe
-function setBreathe() {
-    hue1 = Math.round(breatheColorInput.value * 1.41176);
-    document.documentElement.style.setProperty('--hue-1', hue1);
-
-    hue2 = hue1;
-    document.documentElement.style.setProperty('--hue-2', hue2);
-
-    sat = '100%';
-    document.documentElement.style.setProperty('--sat', sat);
-
-    document.documentElement.style.setProperty('--speed', 10000 - breatheSpeedInput.value + 'ms');
-
-    setBrightness();
-
-    websocket.send("breatheColorValue" + breatheColorInput.value.toString());
-    websocket.send("breatheSpeedValue" + breatheSpeedInput.value.toString());
-}
-
-// Set Motion
-function setMotion() {
-    hue1 = Math.round(motionColorInput.value * 1.41176);
-    document.documentElement.style.setProperty('--hue-1', hue1);
-
-    hue2 = hue1;
-    document.documentElement.style.setProperty('--hue-2', hue2);
-
-    sat = '100%';
-    document.documentElement.style.setProperty('--sat', sat);
-
-    document.documentElement.style.setProperty('--speed', 10000 - motionSpeedInput.value + 'ms');
-
-    setBrightness();
-
-    websocket.send("motionColorValue" + motionColorInput.value.toString());
-    websocket.send("motionSpeedValue" + motionSpeedInput.value.toString());
-}
-
-// Set sparkle
-function setSparkleColor() {
-    hue1 = Math.round(sparkleColorInput.value * 1.41176);
-    document.documentElement.style.setProperty('--hue-1', hue1);
-
-    hue2 = hue1;
-    document.documentElement.style.setProperty('--hue-2', hue2);
-
-    sat = '100%';
-    document.documentElement.style.setProperty('--sat', sat);
-    document.documentElement.style.setProperty('--speed', 10000 - sparkleSpeedInput.value + 'ms');
-
-    setBrightness();
-
-    websocket.send("sparkleColorValue" + sparkleColorInput.value.toString());
-    websocket.send("sparkleSpeedValue" + sparkleSpeedInput.value.toString());
-}
-
-setCurrentMode();
-
-*/
-
